37105907fb9caf34c84eb953d8a213ad12764241
lianxiang118 <924013428@qq.com>
2024-10-04 18:02:27 +0800

    Update 组播综合.py

--- 组播py/组播综合.py
+++ 组播py/组播综合.py
@@ -157,7 +157,7 @@ import replace
 import fileinput
 from opencc import OpenCC
 file_contents = []
-file_paths = ["北京联通.txt","山西联通.txt","内蒙古联通.txt","辽宁联通.txt","江苏电信.txt","湖南电信.txt","陕西电信.txt","四川电信.txt","河南电信.txt","河南联通.txt"]  # 替换为实际的文件路径列表
+file_paths = ["北京联通.txt","山西联通.txt","内蒙古联通.txt","辽宁联通.txt","内蒙古电信.txt","江苏电信.txt","湖南电信.txt","陕西电信.txt","四川电信.txt","河南电信.txt","河南联通.txt"]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     if os.path.exists(file_path):
         with open(file_path, 'r', encoding="utf-8") as file:
@@ -289,7 +289,7 @@ with open('iptv_list.txt', 'r', encoding='utf-8') as file, open('df1.txt', 'w', 
 
 
 ################
-keywords = ['内蒙古','爱上4K','鄂尔多斯']  # 需要提取的关键字列表
+keywords = ['内蒙古','爱上4K','鄂尔多斯','包头','巴彦淖尔','乌拉特前旗']  # 需要提取的关键字列表
 pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
 #pattern = r"^(.*?),(?!#genre#)(.*?)$" #以分类直接复制
 with open('iptv_list.txt', 'r', encoding='utf-8') as file, open('f.txt', 'w', encoding='utf-8') as f:    #####定义临时文件名
